52babb33bb1dc91b60e8fb09b622c165901d5a52
Ivan Doroshenko <ploddi@gmail.com>
2013-11-06 16:23:08 +0400

    Initial Commit

---
+++ Prive/PVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PVAppDelegate.h
+//  Prive
+//
+//  Created by Ivan Doroshenko on 11/6/13.
+//  Copyright (c) 2013 Prive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Prive/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Prive
+//
+//  Created by Ivan Doroshenko on 11/6/13.
+//  Copyright (c) 2013 Prive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PVAppDelegate class]));
+    }
+}
