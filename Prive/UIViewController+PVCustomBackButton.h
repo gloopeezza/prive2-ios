e4ba08fa3aaf0fbaaa0a217fd0b668adf0bed7d8
Ivan Doroshenko <ploddi@gmail.com>
2013-11-13 14:51:03 +0400

    Back button category

---
+++ Prive/UIViewController+PVCustomBackButton.h
@@ -0,0 +1,15 @@
+//
+//  UIViewController+PVCustomBackButton.h
+//  Prive
+//
+//  Created by Ivan Doroshenko on 11/13/13.
+//  Copyright (c) 2013 Prive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (PVCustomBackButton)
+
+- (void)pv_configureBackButton;
+
+@end
