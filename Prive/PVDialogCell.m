71c83622da09e20070765b8fb98ab546ebf793fd
Andrey Tyshlaev <edward@dezigner.ru>
2013-11-13 16:19:49 +0400

    centering balloon

--- Prive/PVDialogCell.m
+++ Prive/PVDialogCell.m
@@ -10,6 +10,8 @@
 #import "UIImage+Appearance.h"
 #import <QuartzCore/QuartzCore.h>
 
+#define TEXT_VIEW_WIDTH 135
+
 @interface PVDialogCell()
 {
     float height;
@@ -43,15 +45,13 @@
 
         self.backgroundColor = UIColor.clearColor;
         
-        self.imageAvatar = [[UIImageView alloc] initWithFrame:CGRectMake(10.0, 0.0, 70, 70)];
+        self.imageAvatar = [[UIImageView alloc] initWithFrame:CGRectMake(0.0, 0.0, 70, 70)];
 
         self.imageBalloon = [[UIImageView alloc] initWithFrame:CGRectZero];
 
         [self.contentView addSubview:self.imageAvatar];
         [self.contentView addSubview:self.imageBalloon];
         [self.contentView addSubview:self.textView];
-
-
     }
     return self;
 }
@@ -68,7 +68,7 @@
     self.textView.frame = CGRectZero;
     self.textView.text = @"";
     self.imageView.image = nil;
-    //self.imageBalloon.image = nil;
+    self.imageBalloon.image = nil;
 }
 
 - (void)setupCellWithType:(PVDialogCellType)typeCell andMessage:(PVManagedMessage *)message
@@ -80,30 +80,31 @@
     CGSize size = [message.text sizeWithFont:[UIFont fontWithName:@"Helvetica" size:14.0]
                            constrainedToSize:CGSizeMake(kMessageTextWidth, CGFLOAT_MAX)
                                lineBreakMode:NSLineBreakByWordWrapping];
-    height = size.height;
-    
-    
-    
+    height = MAX(size.height, 25);
 }
 
 - (void)layoutSubviews {
     [super layoutSubviews];
-    
+
     if (_type == PVDialogCellSent) {
-        self.imageAvatar.center = CGPointMake(40.0, 40.0);
+        self.imageAvatar.center = CGPointMake(40, self.frame.size.height/2);
         [self.textView setFrame:CGRectMake(self.imageAvatar.frame.size.width + 10.0, 10.0, 145.0, height)];
+        [self.textView setCenter:CGPointMake(self.imageAvatar.frame.size.width + 20.0 + (TEXT_VIEW_WIDTH + 10.0)/2, self.frame.size.height/2 + 4)];
         self.imageAvatar.image = [UIImage defaultAvatarWithHeight:60.0 borderColor:[UIColor colorWithRed:45/255.0 green:114/255.0 blue:21.0/255.0 alpha:0.8]];
         if (height > 0) {
-                [_imageBalloon setFrame:CGRectMake(self.imageAvatar.frame.size.width + 10.0, 10, 145, height)];
-                [_imageBalloon setImage:[UIImage balloonImageWithHeight:height backgroundColor:UIColor.whiteColor]];
+            [_imageBalloon setFrame:CGRectMake(0, 0, TEXT_VIEW_WIDTH + 10.0, height)];
+            [_imageBalloon setCenter:CGPointMake(self.imageAvatar.frame.size.width + 10.0 + (TEXT_VIEW_WIDTH + 10.0)/2, self.frame.size.height/2)];
+            [_imageBalloon setImage:[UIImage balloonImageWithHeight:height backgroundColor:UIColor.whiteColor sent:YES]];
         }
     }else{
-        self.imageAvatar.center = CGPointMake(280.0, 40.0);
+        self.imageAvatar.center = CGPointMake(self.frame.size.width - 40, self.frame.size.height/2);
         [self.textView setFrame:CGRectMake(self.bounds.size.width - self.imageAvatar.frame.size.width - 145.0 - 5.0, 10.0, 145.0, height)];
+        [self.textView setCenter:CGPointMake(self.bounds.size.width - self.imageAvatar.frame.size.width - TEXT_VIEW_WIDTH/2 - 20.0 + 10, self.frame.size.height/2 + 4)];
         self.imageAvatar.image = [UIImage defaultAvatarWithHeight:60.0 borderColor:[UIColor colorWithRed:71/255.0 green:98/255.0 blue:115.0/255.0 alpha:0.8]];
         if (height > 0) {
-                [_imageBalloon setFrame:CGRectMake(self.bounds.size.width - self.imageAvatar.frame.size.width - 145.0 - 10.0, 10.0, 145.0, height)];
-                [_imageBalloon setImage:[UIImage balloonImageWithHeight:height backgroundColor:[UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.8]]];
+            [_imageBalloon setFrame:CGRectMake(self.bounds.size.width - self.imageAvatar.frame.size.width - TEXT_VIEW_WIDTH - 10.0, 10.0, 145.0, height)];
+            [_imageBalloon setCenter:CGPointMake(self.bounds.size.width - self.imageAvatar.frame.size.width - TEXT_VIEW_WIDTH/2 - 20.0, self.frame.size.height/2)];
+            [_imageBalloon setImage:[UIImage balloonImageWithHeight:height backgroundColor:[UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.8] sent:NO]];
         }
     }
 }
