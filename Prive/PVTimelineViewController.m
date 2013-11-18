32edb2979698720cecd90141a597a3731078c979
Ivan Doroshenko <ploddi@gmail.com>
2013-11-18 21:17:02 +0400

    Timeline stub

--- Prive/PVTimelineViewController.m
+++ Prive/PVTimelineViewController.m
@@ -21,10 +21,20 @@
         self.title = @"Timeline";
         self.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"Timeline" image:nil tag:0];
         [self.tabBarItem setFinishedSelectedImage:[UIImage imageNamed:@"tabbar-timeline-highlighted"] withFinishedUnselectedImage:[UIImage imageNamed:@"tabbar-timeline-normal"]];
-
+        
     }
     
     return self;
 }
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    CGRect rect = CGRectMake(0.0f, 64.0f, 320.0f, 455.0f);
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:rect];
+    imageView.image = [UIImage imageNamed:@"timelinescreen"];
+    [self.view addSubview:imageView];
+}
+
 
 @end
